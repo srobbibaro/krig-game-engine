1211979dd515f910c106655a28bf4bb310561538
srobbibaro <steverobbibaro@gmail.com>
2008-12-08 07:01:43 +0000

    Slighly better (although not finished) player shot script.

--- scripts/player_shot.lua
+++ scripts/player_shot.lua
@@ -2,7 +2,17 @@ duration = 0.0
 
 function on_load(this)
     setModel(this, "blueshot.mdl")
-    setVelocity(this, 15.0, 0.0, 0.0)
+
+    player = getPlayer()
+    prx, pry, prz = getRotation(player)
+    pdx, pdy, pdz = getDirection(player)
+
+    setRotation(this, prx, pry, prz)
+    setRotationVelocity(this, pdx * 8.0, pdy * 8.0, pdz * 8.0)
+
+    -- logic here needs to be fixed
+    setSpeed(this, 0, 25.0)
+
     return
 end
 
