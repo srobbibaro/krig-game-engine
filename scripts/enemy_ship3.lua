80910de146af7a9ea561e66eb48bf96ada54b21a
timecatalyst <timecatalyst@gmail.com>
2009-02-22 18:07:32 +0000

    New code for sweeping ship animations.

--- scripts/enemy_ship3.lua
+++ scripts/enemy_ship3.lua
@@ -8,6 +8,32 @@ function on_load(this)
     return
 end
 function on_update(this, elapsedTime)
+    this_position = getPosition(this)
+    camera_pos    = getPosition( getCamera() )
+    relative_pos  = this_position[1] - camera_pos[1]
+    if ( relative_pos > 20 ) then return end
+
+    -- Bottom of the curve.
+    if ( this_position[2] < 15 ) then
+        -- Start moving forward.
+        if ( relative_pos < 20 ) then setVelocity(this, 0.0, 0.0, 0.0) end
+        -- Angle a bit up.
+        if ( relative_pos < 10 ) then setVelocity(this, 2.0, 5.0, 0.0) end
+        -- Go straight up.
+        if ( relative_pos < 5 )  then setVelocity(this, 10.0, 10.0, 0.0) end
+        return
+    end
+
+    -- Should be going straight up when we get to here.
+    -- Top of the curve.
+    if ( this_position[2] >= 15 ) then
+        -- Start curving back to the right.
+        if this_position[2] > 20  then setVelocity(this, 18.0, 5.0, 0.0) end
+        -- Full retreat.
+        if relative_pos > 10     then setVelocity(this, 20.0, 0.0, 0.0) end
+        -- Remove when we get way off the screen.
+        if relative_pos > 20     then removeObject(this) end
+    end
     return
 end
 function on_unload(this)
