4b023936bbf8ec14ffc3aa34269939db0aa21856
srobbibaro <steverobbibaro@gmail.com>
2009-10-26 01:18:26 +0000

    Update name of included file to match name of actual header file.

--- src/Sphere.h
+++ src/Sphere.h
@@ -1,7 +1,7 @@
 #ifndef _SPHERE_H_
 #define _SPHERE_H_
 
-#include "Matrix.h"
+#include "matrix.h"
 #include "Vector.h"
 
 class Sphere
@@ -9,13 +9,13 @@ class Sphere
     public:
         Sphere(void);
         ~Sphere(void);
-        
+
         void setSphere(float tX, float tY, float tZ, float tRadius);
-        
+
         void getOriginVector(Vector &v);
         float getRadius(void) const;
         void setOriginVector(Vector &v) { x = v.x; y = v.y; z = v.z; }
-        
+
     private:
         float x, y, z;  // origin
         float radius;
