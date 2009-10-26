41fd18837dfe1cbc928f0a346790b86216b72c0a
srobbibaro <steverobbibaro@gmail.com>
2009-10-26 01:13:59 +0000

    Include required library.

--- src/Snow.cpp
+++ src/Snow.cpp
@@ -1,15 +1,17 @@
+#include <stdlib.h>
+
 #include "Snow.h"
 
 //------------------------------------------------------------------------------
 Snow::Snow(Object* tOrigin)
 {
     origin = tOrigin;
-     
+
     maxParticles = 3000;
     numParticles = 3000;
- 
+
     particles = new Particle[maxParticles];
-    
+
     init();
 }
 
@@ -31,42 +33,42 @@ void Snow::init(void)
 void Snow::draw(void)
 {
     glColor3f( 1.0f, 1.0f, 1.0f );
-    
+
     for (int i = 0; i < numParticles; i++) {
-    
+
               Vector up, normal;
                 up.setVector( 0.0f, 0.0f, 1.0f );
                 normal.setVector((origin->position.x-particles[i].position.x), 0, (origin->position.z-particles[i].position.z));
                 normal.normalize();
-              
+
                 Vector rotationAxis;
                 rotationAxis.crossProduct( up, normal );
                 rotationAxis.normalize();
                 float rotationAngle = up.dotProduct( normal );
 
                 glPushMatrix();
-                
+
                 rotationAngle = acos(rotationAngle);
                 rotationAngle = ((rotationAngle*180)/3.14);
-                
+
                 glTranslatef(particles[i].position.x, particles[i].position.y, particles[i].position.z);
                 glRotatef(rotationAngle, rotationAxis.x, rotationAxis.y, rotationAxis.z);
-                
-                
+
+
                 Vector t;
                 t.setVector((origin->position.x-particles[i].position.x), (origin->position.y-particles[i].position.y), (origin->position.z-particles[i].position.z));
                 t.normalize();
-                
+
                 rotationAngle = normal.dotProduct(t);
-                
+
                 if (t.y < 0)
-			         glRotatef(acos(rotationAngle)*180/3.14,1,0,0);	
+			         glRotatef(acos(rotationAngle)*180/3.14,1,0,0);
                 else
-			         glRotatef(acos(rotationAngle)*180/3.14,-1,0,0);	
-                
-                
-              
-               
+			         glRotatef(acos(rotationAngle)*180/3.14,-1,0,0);
+
+
+
+
         glBegin( GL_TRIANGLES );
             glVertex3f(-0.1, -0.1, 0.0);
             glVertex3f(0.1, -0.1, 0.0);
@@ -84,25 +86,25 @@ void Snow::draw(void)
             glVertex3f(0.14, 0.0, -0.001);
             glVertex3f(0.0, 0.14, -0.001);
         glEnd();
-        
-        glPopMatrix(); 
+
+        glPopMatrix();
     }
 }
 
 //------------------------------------------------------------------------------
 void Snow::initParticle(int index)
-{   
+{
     particles[index].position.setVector(
         origin->position.x +  rand() % 80 - 40,
         origin->position.y +  20.0f + rand() % 10, //rand() %  40 - 20,
         origin->position.z +  rand() % 40 - 40
-    ); 
-    
+    );
+
     particles[index].velocity.setVector(
         -(rand() % 3),
         -(1.0f + rand() % 5),
         0.0f
-    ); 
+    );
 }
 
 //------------------------------------------------------------------------------
@@ -112,14 +114,14 @@ void Snow::update(float elapsedTime)
         particles[i].position.x += particles[i].velocity.x * elapsedTime;
         particles[i].position.y += particles[i].velocity.y * elapsedTime;
         particles[i].position.z += particles[i].velocity.z * elapsedTime;
-        
+
         if ( particles[i].position.x > origin->position.x + 40.0f ||
              particles[i].position.x < origin->position.x - 40.0f ||
              particles[i].position.y > origin->position.y + 30.0f ||
-             particles[i].position.y < origin->position.y - 30.0f 
+             particles[i].position.y < origin->position.y - 30.0f
          )
             initParticle(i);
-        
-        
+
+
     }
 }
