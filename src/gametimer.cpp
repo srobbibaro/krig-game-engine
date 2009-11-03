006ce78166e7ab9cfb15c75da1e9449f2ac6741c
srobbibaro <steverobbibaro@gmail.com>
2009-11-03 01:51:59 +0000

    Linux hack (temp)...

--- src/gametimer.cpp
+++ src/gametimer.cpp
@@ -8,25 +8,45 @@
 //                                                              //
 // Date        : 12-1-03                                        //
 //////////////////////////////////////////////////////////////////
-#include <windows.h>
+//#include <windows.h>
+
+#include <iostream>
+
+#include <sys/time.h>
 #include "gametimer.h"
+
+
+using namespace std;
 
 //------------------------------------------------------------------------------
 bool GameTimer::init(void)
 {
+    /*
   if (!QueryPerformanceFrequency(&m_ticksPerSecond))
+
     return false;  // system doesn't support high-res timer
-  
+
   else
   {
     QueryPerformanceCounter(&m_startTime);
     return true;
   }
+  */
+  timeval tv;
+  gettimeofday(&tv, 0);
+  //m_startTime = ((double)tv.tv_usec / 1000.0 + (double)tv.tv_sec * 1000.0);
+  m_startTime = tv.tv_usec / 1000 + tv.tv_sec * 1000;
+
+  fps = 0.0f;
+
+  return true;
+
 }
 
 //------------------------------------------------------------------------------
 float GameTimer::getElapsedSeconds( unsigned long )
 {
+    /*
   static LARGE_INTEGER s_lastTime = m_startTime;
   LARGE_INTEGER currentTime;
 
@@ -38,11 +58,32 @@ float GameTimer::getElapsedSeconds( unsigned long )
   s_lastTime = currentTime;
 
   return seconds;
-} 
+  */
+
+  timeval tv;
+  gettimeofday(&tv, 0);
+  //double currentTime = ((double)tv.tv_usec / 1000.0 + (double)tv.tv_sec * 1000.0);
+
+  //double seconds = (currentTime - m_startTime) / 1000.0;
+
+   long currentTime = (tv.tv_usec / 1000 + tv.tv_sec * 1000);
+
+  float seconds = float(currentTime - m_startTime) / 1000.0f;
+
+
+  fps = 1.0f / seconds;
+  //  cout << "s: " << m_startTime << " c: " << currentTime << endl;
+  //cout << "seconds: " << seconds << endl;
+
+  m_startTime = currentTime;
+
+  return seconds;
+}
 
 //------------------------------------------------------------------------------
 float GameTimer::getFPS(void)
 {
+    /*
   unsigned long elapsedFrames = 1;
   static LARGE_INTEGER s_lastTime = m_startTime;
   LARGE_INTEGER currentTime;
@@ -52,7 +93,22 @@ float GameTimer::getFPS(void)
   float fps = (float)elapsedFrames * (float)m_ticksPerSecond.QuadPart / ((float)currentTime.QuadPart - (float)s_lastTime.QuadPart);
 
   // reset the timer
+
   s_lastTime = currentTime;
+
+  return fps;
+  */
+
+  /*
+
+  long m_lastTime = m_startTime;
+
+  timeval tv;
+  gettimeofday(&tv, 0);
+  long currentTime = tv.tv_usec / 1000 + (tv.tv_sec * 1000);
+
+  float fps = 1.0f / (float)(currentTime - m_lastTime) * 1000.0f;
+  */
 
   return fps;
 } // end GetFPS
