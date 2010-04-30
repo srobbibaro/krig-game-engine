2122741ac7a4f2161edd75e9a8b829c224fa972a
srobbibaro <steverobbibaro@gmail.com>
2010-04-30 03:19:32 +0000

    ObjectList class is used to manage a list of game objects

---
+++ src/ObjectList.cpp
@@ -0,0 +1,49 @@
+#include "ObjectList.h"
+#include "ObjectNode.h"
+
+ObjectList::ObjectList()
+{
+    head = NULL;
+    size = 0;
+}
+
+ObjectList::~ObjectList()
+{
+    head = NULL;
+}
+
+void ObjectList::insertFront(ObjectNode* objectNode)
+{
+    if (objectNode == NULL)
+        return;
+
+    objectNode->next = head;
+    objectNode->prev = NULL;
+
+    if (head != NULL)
+        head->prev = objectNode;
+
+    head = objectNode;
+
+    size++;
+}
+
+void ObjectList::remove(ObjectNode* objectNode)
+{
+    if (objectNode == NULL)
+        return;
+
+    if (objectNode->prev != NULL)
+        objectNode->prev->next = objectNode->next;
+    else
+        head = objectNode->next;
+
+    if (objectNode->next != NULL)
+        objectNode->next->prev = objectNode->prev;
+
+
+
+    objectNode->prev = objectNode->next = NULL;
+
+    size--;
+}
---
+++ src/ObjectList.h
@@ -0,0 +1,20 @@
+#ifndef _OBJECT_LIST_H_
+#define _OBJECT_LIST_H_
+
+#include "ObjectNode.h"
+#include <constants.h>
+
+class ObjectList
+{
+    public:
+        ObjectList();
+        ~ObjectList();
+
+        void insertFront(ObjectNode*);
+        void remove(ObjectNode*);
+
+        ObjectNode* head;
+        int size;
+};
+
+#endif
