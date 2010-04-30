f62242db2b553e49ccf1f531d285745051abb4cd
srobbibaro <steverobbibaro@gmail.com>
2010-04-30 03:18:45 +0000

    Updated level 4 script.

--- levels/level4.lua
+++ levels/level4.lua
@@ -1,31 +1,35 @@
 dofile("./levels/level_lib.lua")
 
-x_start_camera = 50.0
-x_start_player = x_start_camera - 40.0
+x_start_camera = 180.0 -- 110.0
+x_start_player = x_start_camera - 20.0
+
+bossBattle = 0
+boss = nil
+bossLife = 0
 
 function on_load(terrain)
     setSkyBox(0.0, 0.0, 0.0,
               0.0, 0.0, 0.3,
-	          1.0, 0.0, 0.5)
+              1.0, 0.0, 0.5)
 
-    setLightDirection(0.0, 0.2, 0.8)
+    setLightDirection(0.96, 0.07, 0.13)
 
     setTerrain(terrain, "./terrains/level4.txt")
 
-    player = getPlayer(terrain)
+    player = getPlayer()
     setScript(player, "./scripts/player1.lua")
-    setPosition(player, x_start_player, 15.0, 7.5)
+    setPosition(player, x_start_player, 20.0, 77.5)
 
-    camera = getCamera(obj)
+    camera = getCamera()
     setScript(camera, "./scripts/camera1.lua")
-    setPosition(camera, x_start_camera, 15.0, 27.5)
-
-    playBgMusic("./music/Sparkman.ogg", 1)
+    setPosition(camera, x_start_camera, 15.0, 105.0)
     
+    --playBgMusic("./music/Sparkman.ogg", 1)
+
     return
 end
 
-function on_update()
+function on_update(terrain, elapsedTime)
     return
 end
 
@@ -38,7 +42,8 @@ function on_draw()
 end
 
 function on_draw_screen()
-    display_hud(0,0)
+    display_hud(bossBattle, bossLife)
     display_debug()
     return
 end
+
