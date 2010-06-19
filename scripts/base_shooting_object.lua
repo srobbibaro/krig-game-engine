7ec092d32f04bb30b3e789254d36218e78316d38
srobbibaro <steverobbibaro@gmail.com>
2010-06-19 02:30:11 +0000

    Updated to allow shots with direction specified

--- scripts/base_shooting_object.lua
+++ scripts/base_shooting_object.lua
@@ -86,6 +86,43 @@ function setShotNew(this, obj, dir_offset, up_offset)
     return obj
 end
 
+function setShotWithDirectionNew(this, obj, direction, dir_offset, up_offset)
+    if up_offset == nil then up_offset = 0 end
+    if dir_offset == nil then dir_offset = 0 end
+
+    up = getUp(this)
+    this_position = getPosition(this)
+
+    direction[1] = direction[1] * dir_offset
+    direction[2] = direction[2] * dir_offset
+    direction[3] = direction[3] * dir_offset
+
+    up[1] = up[1] * up_offset
+    up[2] = up[2] * up_offset
+    up[3] = up[3] * up_offset
+
+    this_position[1] = this_position[1] + direction[1] + up[1]
+    this_position[2] = this_position[2] + direction[2] + up[2]
+    this_position[3] = this_position[3] + direction[3] + up[3]
+
+    setPositionv(obj, this_position)
+
+    this_rotation  = getRotation(this)
+    this_direction = getDirection(this)
+    this_velocity  = getVelocity(this)
+
+    --speed = vector_getScalar(this_velocity, this_direction) + 25.0
+    speed = 25
+   
+    setRotationv(obj, this_rotation)
+    setRotationVelocity(obj, this_direction[1] * 8.0, this_direction[2] * 8.0, this_direction[3] * 8.0)
+
+    -- logic here needs to be fixed
+    setSpeed(obj, speed, 0.0, 0.0)
+
+    return obj
+end
+
 function update_shots(elapsedTime)
     next_shot_time = next_shot_time - elapsedTime
 end
@@ -94,6 +131,15 @@ function attemptShot(this, dir_offset, up_offset)
     if next_shot_time <= 0.0 then
         shot = addObject(this, script)
         setShotNew(this, shot, dir_offset, up_offset)
+        next_shot_time = initial_next_shot_time
+        return
+    end
+end
+
+function attemptShotWithDirection(this, direction, dir_offset, up_offset)
+    if next_shot_time <= 0.0 then
+        shot = addObject(this, script)
+        setShotWithDirectionNew(this, shot, direction, dir_offset, up_offset)
         next_shot_time = initial_next_shot_time
         return
     end
