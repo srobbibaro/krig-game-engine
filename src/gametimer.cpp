4073355edbc92c40e863db06f714a7e35eef2190
srobbibaro <steverobbibaro@gmail.com>
2010-06-24 03:33:02 +0000

    Some fixes for linux

--- src/gametimer.cpp
+++ src/gametimer.cpp
@@ -10,8 +10,6 @@
 //////////////////////////////////////////////////////////////////
 //#include <windows.h>
 
-#include <iostream>
-
 #include <sys/time.h>
 #include "gametimer.h"
 
@@ -60,22 +58,29 @@ float GameTimer::getElapsedSeconds( unsigned long )
   return seconds;
   */
 
-  timeval tv;
-  gettimeofday(&tv, 0);
-  //double currentTime = ((double)tv.tv_usec / 1000.0 + (double)tv.tv_sec * 1000.0);
+  fps = 100.0f;
+  float seconds = 0.0f;
 
-  //double seconds = (currentTime - m_startTime) / 1000.0;
+  int tries = 0;
 
-   long currentTime = (tv.tv_usec / 1000 + tv.tv_sec * 1000);
+  while (fps >= 60.0f || ++tries > 100) {
+    timeval tv;
+    gettimeofday(&tv, 0);
+    //double currentTime = ((double)tv.tv_usec / 1000.0 + (double)tv.tv_sec * 1000.0);
 
-  float seconds = float(currentTime - m_startTime) / 1000.0f;
+    //double seconds = (currentTime - m_startTime) / 1000.0;
+
+    long currentTime = (tv.tv_usec / 1000 + tv.tv_sec * 1000);
+
+    seconds += float(currentTime - m_startTime) / 1000.0f;
 
 
-  fps = 1.0f / seconds;
-  //  cout << "s: " << m_startTime << " c: " << currentTime << endl;
-  //cout << "seconds: " << seconds << endl;
+    fps = 1.0f / seconds;
+    //  cout << "s: " << m_startTime << " c: " << currentTime << endl;
+    //cout << "seconds: " << seconds << endl;
 
-  m_startTime = currentTime;
+    m_startTime = currentTime;
+  }
 
   return seconds;
 }
