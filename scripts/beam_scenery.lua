174caee82f759f2063067b9510bf26d62bfac367
timecatalyst <timecatalyst@gmail.com>
2010-06-26 02:17:36 +0000

    Script for beams that act as scenery

---
+++ scripts/beam_scenery.lua
@@ -0,0 +1,18 @@
+function on_load(this)
+    setModel(this, "Beam.mdl")
+    setRotation(this, 0.0, 0.0, 0.0)     
+    disableCollisionDetection(this)
+    return
+end
+
+function on_update(this, elapsedTime)
+    return
+end
+
+function on_unload(this)
+    return
+end
+
+function on_collision(this, temp)
+    return
+end
