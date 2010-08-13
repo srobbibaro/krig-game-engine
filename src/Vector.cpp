4a4dce376e56fbfad097cc0b081d1835e957c905
srobbibaro <steverobbibaro@gmail.com>
2010-08-13 03:26:31 +0000

    Only output console debug when DEBUG macro is enabled

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -22,7 +22,7 @@ void Vector::setVector( float tx, float ty, float tz )
 void Vector::normalize()
 {
     float len = sqrtf( ( x * x ) + ( y * y ) + ( z * z ) );
-    
+
     if ( len != 0.0f ) {
         x /= len;
         y /= len;
@@ -62,13 +62,13 @@ void Vector::crossProduct ( Vector &d1, Vector &d2 )
 void Vector::calcNorm( Vector &p1, Vector &p2, Vector &p3 )
 {
     // calculate the surface normal of the triangle formed by p1, p2 and p3
-    
+
     Vector t1;
     Vector t2;
-    
+
     t1.setVector( p2.x - p1.x, p2.y - p1.y, p2.z - p1.z );
     t2.setVector( p3.x - p1.x, p3.y - p1.y, p3.z - p1.z );
- 
+
     crossProduct( t1, t2 );
     normalize();
 }
@@ -77,12 +77,12 @@ void Vector::calcNorm( Vector &p1, Vector &p2, Vector &p3 )
 void Vector::transformVector( Matrix &m, Vector &v )
 {
     float h;
-    
+
     x = ( v.x * m.data[0] ) + ( v.y * m.data[4] ) + ( v.z * m.data[8] ) + m.data[12];
     y = ( v.x * m.data[1] ) + ( v.y * m.data[5] ) + ( v.z * m.data[9] ) + m.data[13];
     z = ( v.x * m.data[2] ) + ( v.y * m.data[6] ) + ( v.z * m.data[10] ) + m.data[14];
     h = ( v.x * m.data[3] ) + ( v.y * m.data[7] ) + ( v.z * m.data[11] ) + m.data[15];
-        
+
    //x /= h;
    // y /= h;
    // z /= h;
@@ -94,7 +94,7 @@ void Vector::average( Vector &a, Vector &b )
     x = a.x + b.x;
     y = a.y + b.y;
     z = a.z + b.z;
-    
+
     normalize();
 }
 
@@ -127,92 +127,92 @@ float Vector::getScaler(Vector vector)
     t.x = x * vector.x;
     t.y = y * vector.y;
     t.z = z * vector.z;
-    
+
     return (t.x + t.y + t.z);
 }
 
 //------------------------------------------------------------------------------
-bool Vector::intersectBox( 
-    Vector rayPosition, Vector collisionBox[], 
-    float extend, Vector &tv 
+bool Vector::intersectBox(
+    Vector rayPosition, Vector collisionBox[],
+    float extend, Vector &tv
  )
 {
     bool hit = false;
     float s = 0;
-           
+
     // test planes with fixed x value //
     if ( x > 0 ) {
         if ( rayPosition.x <= collisionBox[1].x+extend )
-            s = (collisionBox[1].x+extend-rayPosition.x) / x;           
+            s = (collisionBox[1].x+extend-rayPosition.x) / x;
     }
     else if ( x < 0 ) {
         if ( rayPosition.x >= collisionBox[0].x-extend )
             s = (collisionBox[0].x-extend-rayPosition.x) / x;
     }
-                                 
-            
+
+
     if ( s != 0 ) {
         tv.x = rayPosition.x +x * s;
         tv.y = rayPosition.y +y * s;
         tv.z = rayPosition.z +z * s;
-                
-        if ( tv.y <= collisionBox[1].y+extend && tv.y >= collisionBox[0].y-extend && 
+
+        if ( tv.y <= collisionBox[1].y+extend && tv.y >= collisionBox[0].y-extend &&
              tv.z <= collisionBox[1].z+extend && tv.z >= collisionBox[0].z-extend) {
                 hit = true;
                 }
             }
     //////////////////////////////////////
-            
-            
+
+
     // if no hit on x planes test planes with fixed y value //
     if ( !hit ) {
         s = 0;
-                
+
         if ( y > 0 ) {
             if ( rayPosition.y <= collisionBox[1].y+extend )
                 s = (collisionBox[1].y+extend-rayPosition.y) / y;
-              
+
         }
         else if ( y < 0 ) {
             if ( rayPosition.y >= collisionBox[0].y-extend )
-                s = (collisionBox[0].y-extend-rayPosition.y) / y;           
+                s = (collisionBox[0].y-extend-rayPosition.y) / y;
         }
-            
+
         if ( s != 0 ) {
-            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend && 
+            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend &&
                  tv.z <= collisionBox[1].z+extend && tv.z >= collisionBox[0].z-extend) {
                  hit = true;
             }
         }
     }
     ////////////////////////////////////////////////////////////
-            
+
     // if no hit on x or y planes test planes with fixed z value //
     if ( !hit ) {
         s = 0;
-                
+
         if ( z > 0 ) {
             if ( rayPosition.z <= collisionBox[1].z+extend )
-                s = (collisionBox[1].z+extend-rayPosition.z) / z;   
+                s = (collisionBox[1].z+extend-rayPosition.z) / z;
         }
         else if ( z < 0 ) {
             if ( rayPosition.z >= collisionBox[0].z-extend )
-                s = (collisionBox[0].z-extend-rayPosition.z) / z;  
+                s = (collisionBox[0].z-extend-rayPosition.z) / z;
         }
-            
+
         if ( s != 0 ) {
             tv.x = rayPosition.x +x * s;
             tv.y = rayPosition.y +y * s;
-            tv.z = rayPosition.z +z * s;          
-                    
-            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend && 
+            tv.z = rayPosition.z +z * s;
+
+            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend &&
                  tv.y <= collisionBox[1].y+extend && tv.y >= collisionBox[0].y-extend) {
                     hit = true;
-            }  
+            }
         }
     }
     ///////////////////////////////////////////////////////////////////
-            
+
     return hit;
 }
 
@@ -222,79 +222,79 @@ bool Vector::intersectBox( Vector rayPosition, Vector collisionBox[], float exte
     Vector tv;
     bool hit = false;
     float s = 0;
-           
+
     // test planes with fixed x value //
     if ( x > 0 ) {
         if ( rayPosition.x <= collisionBox[1].x+extend )
-            s = (collisionBox[1].x+extend-rayPosition.x) / x;           
+            s = (collisionBox[1].x+extend-rayPosition.x) / x;
     }
     else if ( x < 0 ) {
         if ( rayPosition.x >= collisionBox[0].x-extend )
             s = (collisionBox[0].x-extend-rayPosition.x) / x;
-    }                             
-            
+    }
+
     if ( s != 0 ) {
         tv.x = rayPosition.x +x * s;
         tv.y = rayPosition.y +y * s;
         tv.z = rayPosition.z +z * s;
-                
-        if ( tv.y <= collisionBox[1].y+extend && tv.y >= collisionBox[0].y-extend && 
+
+        if ( tv.y <= collisionBox[1].y+extend && tv.y >= collisionBox[0].y-extend &&
              tv.z <= collisionBox[1].z+extend && tv.z >= collisionBox[0].z-extend) {
                 hit = true;
                 }
             }
     //////////////////////////////////////
-            
-            
+
+
     // if no hit on x planes test planes with fixed y value //
     if ( !hit ) {
         s = 0;
-                
+
         if ( y > 0 ) {
             if ( rayPosition.y <= collisionBox[1].y+extend )
                 s = (collisionBox[1].y+extend-rayPosition.y) / y;
-              
+
         }
         else if ( y < 0 ) {
             if ( rayPosition.y >= collisionBox[0].y-extend )
-                s = (collisionBox[0].y-extend-rayPosition.y) / y;           
+                s = (collisionBox[0].y-extend-rayPosition.y) / y;
         }
-            
+
         if ( s != 0 ) {
-            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend && 
+            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend &&
                  tv.z <= collisionBox[1].z+extend && tv.z >= collisionBox[0].z-extend) {
                  hit = true;
             }
         }
     }
     ////////////////////////////////////////////////////////////
-            
+
     // if no hit on x or y planes test planes with fixed z value //
     if ( !hit ) {
         s = 0;
-                
+
         if ( z > 0 ) {
             if ( rayPosition.z <= collisionBox[1].z+extend )
-                s = (collisionBox[1].z+extend-rayPosition.z) / z;   
+                s = (collisionBox[1].z+extend-rayPosition.z) / z;
         }
         else if ( z < 0 ) {
             if ( rayPosition.z >= collisionBox[0].z-extend )
-                s = (collisionBox[0].z-extend-rayPosition.z) / z;  
+                s = (collisionBox[0].z-extend-rayPosition.z) / z;
         }
-            
+
         if ( s != 0 ) {
             tv.x = rayPosition.x +x * s;
             tv.y = rayPosition.y +y * s;
-            tv.z = rayPosition.z +z * s;          
-                    
-            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend && 
+            tv.z = rayPosition.z +z * s;
+
+            if ( tv.x <= collisionBox[1].x+extend && tv.x >= collisionBox[0].x-extend &&
                  tv.y <= collisionBox[1].y+extend && tv.y >= collisionBox[0].y-extend) {
                     hit = true;
-            }  
+            }
         }
     }
     ///////////////////////////////////////////////////////////////////
-            
+
     return hit;
 }
 
@@ -309,7 +309,7 @@ Vector Vector::operator +( Vector a )
 //------------------------------------------------------------------------------
 Vector Vector::operator * ( float a )
 {
-    return Vector( a*x, a*y, a*z );   
+    return Vector( a*x, a*y, a*z );
 }
 
 //------------------------------------------------------------------------------
@@ -334,14 +334,14 @@ float Vector::getLength(void)
 void Matrix::transformVertex( float*v , float *newVertex)
 {
     float h, x, y, z;
-    
+
     x = ( v[0] * data[0] ) + ( v[1] * data[4] ) + ( v[2] * data[8] ) + data[12];
     y = ( v[0] * data[1] ) + ( v[1] * data[5] ) + ( v[2] * data[9] ) + data[13];
     z = ( v[0] * data[2] ) + ( v[1] * data[6] ) + ( v[2] * data[10] ) + data[14];
     h = ( v[0] * data[3] ) + ( v[1] * data[7] ) + ( v[2] * data[11] ) + data[15];
-        
+
     newVertex[0] = x; newVertex[1] = y; newVertex[2] = z;
-    
+
     newVertex[0] /= h;
     newVertex[1] /= h;
     newVertex[2] /= h;
@@ -351,12 +351,12 @@ void Matrix::transformVertex( float*v , float *newVertex)
 void Matrix::setShadow( float lightPos[4], float plane[4] )
 {
     float temp = plane[0] * lightPos[0] + plane[1] * lightPos[1] + plane[1] * lightPos[2] + plane[3] * lightPos[3];
-    
+
     data[0] = temp - lightPos[0] * plane[0];
     data[4] = 0.0f - lightPos[0] * plane[1];
     data[8] = 0.0f - lightPos[0] * plane[2];
     data[12] = 0.0f - lightPos[0] * plane[3];
-    
+
     data[1] = 0.0f - lightPos[1] * plane[0];
     data[5] = temp - lightPos[1] * plane[1];
     data[9] = 0.0f - lightPos[1] * plane[2];
@@ -366,7 +366,7 @@ void Matrix::setShadow( float lightPos[4], float plane[4] )
     data[6] = 0.0f - lightPos[2] * plane[1];
     data[10] = temp - lightPos[2] * plane[2];
     data[14] = 0.0f - lightPos[2] * plane[3];
-    
+
     data[3] = 0.0f - lightPos[3] * plane[0];
     data[7] = 0.0f - lightPos[3] * plane[1];
     data[11] = 0.0f - lightPos[3] * plane[2];
@@ -376,6 +376,7 @@ void Matrix::setShadow( float lightPos[4], float plane[4] )
 //------------------------------------------------------------------------------
 void Matrix::display( void )
 {
+#if DEBUG
     printf("\n");
     for ( int i = 0; i < 16; i++ )
     {
@@ -383,6 +384,7 @@ void Matrix::display( void )
         if ( (i+1) % 4 == 0 )
             printf( "\n" );
     }
+#endif
 }
 
 //------------------------------------------------------------------------------
@@ -392,20 +394,20 @@ void Matrix::setRotation2( float x, float y, float z )
     float cosX = cos(x/2);
     float cosY = cos(y/2);
     float cosZ = cos(z/2);
-    
+
     float sinX = sin(x/2);
     float sinY = sin(y/2);
     float sinZ = sin(z/2);
-    
+
     float cosFinal = cosY * cosZ;
     float sinFinal = sinY * sinZ;
-    
+
     // build the quaternion //
     float quatW = cosX * cosFinal + sinX * sinFinal;
 	float quatX = sinX * cosFinal - cosX * sinFinal;
 	float quatY = cosX * sinY * cosZ + sinX * cosY * sinZ;
 	float quatZ = cosX * cosY * sinZ - sinX * sinY * cosZ;
-    
+
     float wx, wy, wz, xx, yy, yz, xy, xz, zz, x2, y2, z2;
 
     // calculate coefficients
