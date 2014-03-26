9760ceca0f8f6be07655264b8400044f15750e7c
srobbibaro <steverobbibaro@gmail.com>
2014-03-25 23:21:26 -0400

    Update engine splash screen information

--- src/buffer.h
+++ src/buffer.h
@@ -24,7 +24,7 @@ gl.PushMatrix() \
 gl.LineWidth(3.0) \
 gl.Color(0.8, 0.8, 0.8, alpha) \
 displayText(\"Krig Game Engine\", -0.6, 0.1, -0.01, 0.001, 0.001) \
-displayText(\"2008-2014\", -0.2, -0.1, -0.01, 0.0005, 0.001)  \
+displayText(\"2007-2014\", -0.2, -0.1, -0.01, 0.0005, 0.001)  \
 gl.PopMatrix() \
 gl.LineWidth(3.0) \
 return \
