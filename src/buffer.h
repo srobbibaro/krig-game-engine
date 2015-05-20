3a89e0e46cb65d4aebf669b49b62cd076e424def
Steve Robbibaro <steverobbibaro@gmail.com>
2015-05-20 00:49:02 +0000

    Define engine intro buffer in easier to read style
    By using compiler string literal concatenation, we can define the script in an
    equivalent number of characters while allowing for indentation which should make
    the code easier to read and manipulate.

--- src/buffer.h
+++ src/buffer.h
@@ -1,32 +1,40 @@
 #ifndef _BUFFER_H_
 #define _BUFFER_H_
 const char *intro_script_buffer =
-"local alpha = 0.0 \
-function on_load() \
-return \
-end \
-function on_unload() \
-return \
-end \
-function on_update(this) \
-return \
-end \
-function on_draw() \
-return \
-end \
-function on_draw_screen(this, elapsedTime) \
-alpha = alpha + (elapsedTime / 2.0) \
-if alpha >= 2.0 then \
-setComplete(1) \
-end \
-gl.Translate (0.0, 0.0, -2.0) \
-gl.PushMatrix() \
-gl.LineWidth(3.0) \
-gl.Color(0.8, 0.8, 0.8, alpha) \
-displayText(\"Krig Game Engine\", -0.6, 0.1, -0.01, 0.001, 0.001) \
-displayText(\"2007-2015\", -0.2, -0.1, -0.01, 0.0005, 0.001)  \
-gl.PopMatrix() \
-gl.LineWidth(3.0) \
-return \
-end";
+  "local alpha = 0.0\n"
+
+  "function on_load()\n"
+    "return\n"
+  "end\n"
+
+  "function on_unload()\n"
+    "return\n"
+  "end\n"
+
+  "function on_update(this)\n"
+    "return\n"
+  "end\n"
+
+  "function on_draw()\n"
+    "return\n"
+  "end\n"
+
+  "function on_draw_screen(this, elapsedTime)\n"
+    "alpha = alpha + (elapsedTime / 2.0)\n"
+    "if alpha >= 2.0 then\n"
+      "setComplete(1)\n"
+    "end\n"
+
+    "gl.Translate (0.0, 0.0, -2.0)\n"
+    "gl.PushMatrix()\n"
+    "gl.LineWidth(3.0)\n"
+    "gl.Color(0.8, 0.8, 0.8, alpha)\n"
+
+    "displayText(\"Krig Game Engine\", -0.6, 0.1, -0.01, 0.001, 0.001)\n"
+    "displayText(\"2007-2015\", -0.2, -0.1, -0.01, 0.0005, 0.001)\n"
+
+    "gl.PopMatrix()\n"
+    "gl.LineWidth(3.0)\n"
+    "return\n"
+  "end\n";
 #endif
