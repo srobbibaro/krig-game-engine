ab1310716fb50d106929a8ddf28bf8373602d508
Steve Robbibaro <steverobbibaro@gmail.com>
2015-05-20 02:52:28 +0000

    Clean-up KeyState class and move "magic numbers" into defined constants

--- src/KeyState.cpp
+++ src/KeyState.cpp
@@ -1,15 +1,14 @@
 #include "KeyState.h"
 
-void KeyState::initKeyState()
-{
-    for (int i = 0; i < MAX_KEYS; i++)
-        keys[i] = -1;
+void KeyState::initKeyState() {
+  for (int i = 0; i < MAX_KEYS; i++)
+    keys[i] = KEY_STATE_INIT;
 }
 
 bool KeyState::testKeyReleased(int key) {
-    return key >= 0 && key < MAX_KEYS && keys[key] == 2;
+  return key >= 0 && key < MAX_KEYS && keys[key] == KEY_STATE_RELEASED;
 }
 
 bool KeyState::testKeyPressed(int key) {
-    return key >= 0 && key < MAX_KEYS && keys[key] == 1;
+  return key >= 0 && key < MAX_KEYS && keys[key] == KEY_STATE_PRESSED;
 }
--- src/KeyState.h
+++ src/KeyState.h
@@ -1,7 +1,10 @@
 #ifndef _KEY_STATE_H_
 #define _KEY_STATE_H_
 
-#define MAX_KEYS 256
+#define MAX_KEYS           256
+#define KEY_STATE_INIT     -1
+#define KEY_STATE_PRESSED  1
+#define KEY_STATE_RELEASED 2
 
 class KeyState {
   public:
