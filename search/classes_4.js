177031600068200895be523eac686335cca0067a
Steve Robbibaro <steverobbibaro@gmail.com>
2015-08-20 12:07:10 -0400

    Update docs to include changes made to Lua API
    Considerations: Grouping, ordering, etc.

--- search/classes_4.js
+++ search/classes_4.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['gamelevel',['GameLevel',['../classGameLevel.html',1,'']]],
-  ['gametimer',['GameTimer',['../classGameTimer.html',1,'']]],
-  ['gl_5fstr_5fvalue',['gl_str_value',['../structgl__str__value.html',1,'']]]
+  ['gametimer',['GameTimer',['../classGameTimer.html',1,'']]]
 ];
