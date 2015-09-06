c8643344bd3aac1e5dc115f671da0d0ca36df7b9
Steve Robbibaro <steverobbibaro@gmail.com>
2015-09-06 00:29:17 -0400

    Begin to add tests for game level

---
+++ test/gamelevel.cpp
@@ -0,0 +1,65 @@
+#include "catch.hpp"
+#include "GameLevel.h"
+#include <stdio.h>
+
+SCENARIO( "GameLevel", "[GameLevel]" ) {
+  GIVEN( "A game level instance" ) {
+    GameLevel l(1);
+
+    WHEN( "instantiated" ) {
+      THEN( "it is not complete" ) {
+        REQUIRE(l.checkComplete() == false);
+      }
+
+      THEN( "it has no player" ) {
+        REQUIRE(l.getPlayer() == NULL);
+      }
+
+      THEN( "it has no camera" ) {
+        REQUIRE(l.getCamera() == NULL);
+      }
+
+      THEN( "it has no terrain" ) {
+        REQUIRE(l.getTerrain() == NULL);
+      }
+    }
+
+    WHEN( "loading a level without specifying a file" ) {
+      bool result = l.loadLevel("");
+
+      THEN( "it will not load" ) {
+        REQUIRE(result == false);
+      }
+    }
+
+    WHEN( "loading a level without specifying a buffer" ) {
+      bool result = l.loadLevelFromBuffer("");
+
+      THEN( "it will not load" ) {
+        REQUIRE(result == false);
+      }
+    }
+
+    WHEN( "loading a level with a valid buffer" ) {
+      Camera c;
+      l.setCamera(&c);
+      bool result = l.loadLevelFromBuffer("test=1");
+
+      THEN( "it will oad" ) {
+        REQUIRE(result == true);
+      }
+
+      THEN( "it has a player" ) {
+        REQUIRE(l.getPlayer() != NULL);
+      }
+
+      THEN( "it has a camera" ) {
+        REQUIRE(l.getCamera() == &c);
+      }
+
+      THEN( "it has a terrain" ) {
+        REQUIRE(l.getTerrain() != NULL);
+      }
+    }
+  }
+}
