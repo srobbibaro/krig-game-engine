64225f1c590be325ea87dd3deeb1a5863e3c0fab
Steve Robbibaro <steverobbibaro@gmail.com>
2015-09-24 22:10:38 -0400

    Update volcano scripts to use new API functions and style

--- krig3/scripts/lavarock2.lua
+++ krig3/scripts/lavarock2.lua
@@ -5,35 +5,35 @@ local falling = 0
 
 -- Overridden Engine Callbacks
 function on_load(this)
-  krig.object.set_model(this, "redrock.mdl")
-  krig.object.set_scale(this, 4.0, 4.0, 4.0)
-  krig.object.set_rotation(this, 0.0, 0.0, 0.0)
-
-  krig.object.set_type_id(this, 4)
+  this:set_model("redrock.mdl")
+  this.rotation = krig.rotation.from_euler({0.0, 0.0, 0.0})
+  this.scale    = {4.0, 4.0, 4.0}
+  this.type_id  = 4
+  this:save()
 end
 
 function on_update(this, elapsedTime)
-  camera          = krig.get_camera()
-  this_position   = krig.object.get_position(this)
-  camera_position = krig.object.get_position(camera)
+  this   = this:load()
+  camera = krig.get_camera():load()
 
   -- Don't start moving until we're close to it.
-  if camera_position[1] < this_position[1]-30.0 or
-     camera_position[1] > this_position[1]+30.0 then
+  if camera.position[1] < this.position[1]-30.0 or
+     camera.position[1] > this.position[1]+30.0 then
     return
   end
 
   -- Set seed for random positions and velocities.
-  math.randomseed( this_position[1] )
+  math.randomseed( this.position[1] )
 
   -- Stupid line for Windows... Throws out the first (faulty) random value.
   math.random(40);
 
-  krig.object.set_rotation_velocity(this,
-  math.random(0,3), math.random(0,3), math.random(0,3))
+  -- Setup our rotation.
+  this.rotation_velocity = {math.random(0,3), math.random(0,3), math.random(0,3)}
 
   if falling == 1 then
-    krig.object.set_velocity(this, 0.0, -20.0, 0.0)
+    this.velocity = {0.0, -20.0, 0.0}
+    this:save()
     return
   end
 
@@ -41,18 +41,19 @@ function on_update(this, elapsedTime)
   fall_height  = 90  -- Move the rock over to the player when we get here
 
   x_vel = 0.0
-  if this_position[2] >= angle_height then
+  if this.position[2] >= angle_height then
     x_vel = -5.0
   end
-  krig.object.set_velocity(this, x_vel, 15.0, 0.0)
+  this.velocity = {x_vel, 15.0, 0.0}
 
-  if this_position[2] >= fall_height then
+  if this.position[2] >= fall_height then
     falling = 1
-    math.randomseed( camera_position[1] )
-    x_pos = camera_position[1] + math.random(-5,20)
-    krig.object.set_position(this, x_pos, 30.0, 7.5)
-    krig.object.set_scale(this, 3.0, 3.0, 3.0)
+    math.randomseed( camera.position[1] )
+    x_pos = camera.position[1] + math.random(-5,20)
+    this.position = {x_pos, 30.0, 7.5}
+    this.scale    = {3.0, 3.0, 3.0}
   end
+  this:save()
 end
 
 on_collision = asteroid.on_collision
--- krig3/scripts/volcano.lua
+++ krig3/scripts/volcano.lua
@@ -1,31 +1,35 @@
+local scenery = require 'scripts/scenery'
+
 -- Configuration
 local shake = 0
 
 -- Overridden Engine Callbacks
-function on_load(this)
-  krig.object.set_model(this, "volcano.mdl")
-  krig.object.set_scale(this, 20.0, 20.0, 20.0)
-  krig.object.set_rotation(this, 0.0, 1.0, 0.0)
-  krig.object.disable_collision_detection(this)
+function on_load(this, options)
+  this:set_model("volcano.mdl")
+  this.rotation = krig.rotation.from_euler({0.0, 1.0, 0.0})
+  this.scale    = {20.0, 20.0, 20.0}
+  scenery.on_load(this, options)
+  this:save()
 end
 
 function on_update(this, elapsedTime)
-  local camera          = krig.get_camera()
-  local this_position   = krig.object.get_position(this)
-  local camera_position = krig.object.get_position(camera)
+  this   = this:load()
+  camera = krig.get_camera():load()
 
   -- Don't start shaking until we're close to it.
-  if camera_position[1] < this_position[1]-30.0 or
-     camera_position[1] > this_position[1]+30.0 then
+  if camera.position[1] < this.position[1]-30.0 or
+     camera.position[1] > this.position[1]+30.0 then
     return
   end
 
   -- We're in range, shake it baby.
   if shake == 0 then
-    krig.object.set_scale(this, 21.0, 21.0, 21.0);
+    this.scale = {21.0, 21.0, 21.0}
     shake = 1
   else
-    krig.object.set_scale(this, 20.0, 20.0, 20.0);
+    this.scale = {20.0, 20.0, 20.0}
     shake = 0
   end
+
+  this:save()
 end
