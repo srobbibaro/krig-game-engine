e12bd68e6a08f7ec0861adacdb8797f1ae41cfe3
Steve Robbibaro <steverobbibaro@gmail.com>
2015-09-24 22:12:51 -0400

    Update shot/shooting scripts for new API functions and style

--- krig3/scripts/player_missile.lua
+++ krig3/scripts/player_missile.lua
@@ -1,72 +1,54 @@
 -- Configuration
 local duration = 0.0
 local obj      = nil
-local changed  = 0
 
 -- Overridden Engine Callbacks
 function on_load(this)
-  krig.object.set_model(this, "Missle.mdl")
-  krig.object.set_type_id(this, 2)
-  krig.object.enable_always_lit(this)
+  this:set_model("Missle.mdl")
+  this.type_id           = 2
+  this.always_lit        = true
+  this.rotation_velocity = {0.0, 0.0, 0.0}
+  this.speed             = {2.0, 0.0, 0.0}
+  this:save()
 
   krig.play_sound(this, "laser.wav")
 
   obj = krig.level.find_object_of_type(1)
-  krig.object.set_rotation_velocity(this, 0.0, 0.0, 0.0)
 end
 
 function on_update(this, elapsedTime)
+  this = this:load()
+
   duration = duration + elapsedTime
 
   if duration > 5.0 then
     krig.level.remove_object(this)
     duration = 0.0
+    return
   end
-
-  this_dir = krig.object.get_direction(this)
-  this_rot_vel = krig.object.get_rotation_velocity(this)
-  this_speed = krig.object.get_speed(this)
-  this_orth  = krig.object.get_orthogonal(this)
 
   if obj.id >= 0 then
-    krig.object.set_speed(this, 0.0, 0.0, 0.0)
+    obj = obj:load()
 
-    obj_pos = krig.object.get_position(obj)
-    obj_radius = krig.object.get_bounding_sphere_radius(obj)
-    this_pos = krig.object.get_position(this)
+    this.speed = {this.speed[1] + 2.0 * elapsedTime, 0.0, 0.0}
+    this.rotation_velocity  = krig.vector.scale(this.direction, 8.0)
 
-    if obj_pos[2] + obj_radius < this_pos[2] then
-      this_rot_vel[3] = -30.0 * elapsedTime
-      this_rot_vel[2] = 0.0
-      this_rot_vel[1] = 0.0
-      this_speed[1]   = 20.0
-      changed         = 1
-    elseif obj_pos[2] - obj_radius > this_pos[2] then
-      this_rot_vel[3] = 30.0 * elapsedTime
-      this_rot_vel[2] = 0.0
-      this_rot_vel[1] = 0.0
-      this_speed[1]   = 20.0
-      changed         = 2
-    else
-      this_rot_vel  = { this_dir[1] * 8.0, this_dir[2] * 8.0, this_dir[3] * 8.0 }
-      this_speed[1] = this_speed[1] + 8.0 * elapsedTime
-      changed       = 0
+    if obj.position[2] + obj.bounding_sphere_radius < this.position[2] then
+      this.rotation_velocity = {0.0, 0.0, (-30.0 * elapsedTime)}
+      --this.speed[1]          = 20.0
+    elseif obj.position[2] - obj.bounding_sphere_radius > this.position[2] then
+      this.rotation_velocity = {0.0, 0.0, (30.0 * elapsedTime)}
+      --this.speed[1]          = 20.0
     end
   else
-    obj           = krig.level.find_object_of_type(1)
-    this_rot_vel  = { this_dir[1] * 8.0, this_dir[2] * 8.0, this_dir[3] * 8.0 }
-    this_speed[1] = this_speed[1] + 8.0 * elapsedTime
-    changed       = 0
+    obj = krig.level.find_object_of_type(1)
   end
 
-  krig.object.set_rotation_velocity(this, this_rot_vel)
-  krig.object.set_speed(this, this_speed)
+  this:save()
 end
 
 function on_collision(this, temp)
-  typeId = krig.object.get_type_id(temp)
-
-  if typeId ~= 0 then
+  if temp:load().type_id ~= 0 then
     krig.level.remove_object(this)
     duration = 0.0
   end
--- krig3/scripts/shooting_object.lua
+++ krig3/scripts/shooting_object.lua
@@ -22,46 +22,24 @@ local function attemptShots(this, num_shots, dir_offset, up_offset)
   if shooting_object.next_shot_time <= 0.0 then
     for i = 1, num_shots do
       shot = krig.level.add_object(shooting_object.script)
-      setShotNew(this, shot, dir_offset[i], up_offset[i])
+      setShot(this, shot, dir_offset[i], up_offset[i])
     end
     shooting_object.next_shot_time = shooting_object.initial_next_shot_time
   end
 end
 
-function setShotNew(this, obj, dir_offset, up_offset)
-  if up_offset == nil then up_offset = 0 end
-  if dir_offset == nil then dir_offset = 0 end
+function setShot(this, obj, dir_offset, up_offset)
+  up_offset  = up_offset or 0
+  dir_offset = dir_offset or 0
 
-  local direction     = krig.object.get_direction(this)
-  local up            = krig.object.get_up(this)
-  local this_position = krig.object.get_position(this)
-  local speed         = 25
+  local direction = krig.vector.scale(this.direction, dir_offset)
+  local up        = krig.vector.scale(this.up, up_offset)
 
-  direction[1] = direction[1] * dir_offset
-  direction[2] = direction[2] * dir_offset
-  direction[3] = direction[3] * dir_offset
-
-  up[1] = up[1] * up_offset
-  up[2] = up[2] * up_offset
-  up[3] = up[3] * up_offset
-
-  this_position[1] = this_position[1] + direction[1] + up[1]
-  this_position[2] = this_position[2] + direction[2] + up[2]
-  this_position[3] = this_position[3] + direction[3] + up[3]
-
-  krig.object.set_position(obj, this_position)
-
-  this_rotation  = krig.object.get_rotation(this)
-  this_direction = krig.object.get_direction(this)
-  this_velocity  = krig.object.get_velocity(this)
-
-  krig.object.set_rotation(obj, this_rotation)
-  krig.object.set_rotation_velocity(obj, this_direction[1] * 8.0, this_direction[2] * 8.0, this_direction[3] * 8.0)
-
-  -- TODO: logic here needs to be fixed
-  krig.object.set_speed(obj, speed, 0.0, 0.0)
-
-  return obj
+  obj.position          = krig.vector.add(krig.vector.add(this.position, direction), up)
+  obj.rotation          = krig.rotation.copy(this.rotation)
+  obj.rotation_velocity = krig.vector.scale(this.direction, 8.0)
+  obj.speed             = {25.0, 0.0, 0.0}
+  obj:save()
 end
 
 shooting_object = {
