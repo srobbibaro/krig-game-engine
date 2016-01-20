68b06f2bf839ee354c78087128c9a6a43fe032b0
Steve Robbibaro <steverobbibaro@gmail.com>
2016-01-19 21:20:16 -0500

    Update copyright years to include 2016

--- src/buffer.h
+++ src/buffer.h
@@ -15,7 +15,7 @@ const char *intro_level_script_buffer =
     "gl.Color(0.8, 0.8, 0.8, alpha)\n"
 
     "krig.display_text(\"Krig Game Engine\", -0.6, 0.1, -0.01, 0.001, 0.001)\n"
-    "krig.display_text(\"2007-2015\", -0.2, -0.1, -0.01, 0.0005, 0.001)\n"
+    "krig.display_text(\"2007-2016\", -0.2, -0.1, -0.01, 0.0005, 0.001)\n"
 
     "gl.PopMatrix()\n"
     "gl.LineWidth(3.0)\n"
