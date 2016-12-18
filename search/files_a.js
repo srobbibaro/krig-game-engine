e934a2c921daaf52c66ae09e48d9b2dd23459977
travis-ci <steverobbibaro+travisci@gmail.com>
2016-12-18 15:19:33 +0000

    Update documentation after successful master build: 98

---
+++ search/files_a.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['snow_2eh',['Snow.h',['../Snow_8h.html',1,'']]],
+  ['sphere_2eh',['Sphere.h',['../Sphere_8h.html',1,'']]],
+  ['starfield_2eh',['StarField.h',['../StarField_8h.html',1,'']]]
+];
--- search/files_b.js
+++ search/files_b.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['vector',['Vector',['../structVector.html',1,'']]],
   ['vector_2eh',['Vector.h',['../Vector_8h.html',1,'']]]
 ];
